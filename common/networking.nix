fbd27dcc58d8473bf7bbc5b9654927da52a783ac
Paul Dettorer Hervot <dettorer@dettorer.net>
2021-08-25 18:56:01 +0200

    networking: add host names for my home network and vpn

--- common/networking.nix
+++ common/networking.nix
@@ -1,5 +1,20 @@
-{ ... }:
+{ machineName, ... }:
 
 {
   networking.networkmanager.enable = true;
+  networking.extraHosts = ''
+    # Extra names for localhost
+    127.0.0.1	${machineName}.dettorer.net ${machineName}
+    ::1		${machineName}.dettorer.net ${machineName}
+
+    # Magnet VPN
+    10.8.0.1	doublonville.magnet	doublonville
+    10.8.0.10	frimapic.magnet		frimapic
+    10.8.0.30	rivamar.magnet		rivamar
+
+    # Local home network
+    192.168.1.14	frimapic.home	frimapic.home
+    192.168.1.10	loceane.home	loceane.home
+    192.168.1.12	rivamar.home	rivamar.home
+  '';
 }
