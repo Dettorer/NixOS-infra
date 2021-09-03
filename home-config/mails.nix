70f48b33f5bea3e82e57998bd5bc7c9bd5911ac2
Paul Dettorer Hervot <dettorer@dettorer.net>
2021-09-03 14:24:57 +0200

    mail: add signatures

--- home-config/mails.nix
+++ home-config/mails.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
-{
+rec {
   home.packages = with pkgs; [ thunderbird protonmail-bridge ];
 
   # Thunderbird
@@ -22,6 +22,32 @@
       source = ../secrets/thunderbird/profile;
       recursive = true;
     };
+
+    # signatures
+    ".signature" = home.file.".signature.paulhervot";
+    ".signature.dettorer".text = ''
+      Dettorer
+    '';
+    ".signature.paulhervot".text = ''
+      Paul Hervot
+    '';
+    ".signature.epita".text = ''
+      Paul Hervot
+      Enseignant Archi/C-UNIX/ASM
+      EPITA Strasbourg
+    '';
+    ".signature.unistra".text = ''
+      Paul Hervot
+      Étudiant en M2 Sciences de l'Éducation, parcours SYNVA
+    '';
+    ".signature.genepistras".text = ''
+      Paul Hervot
+      pour le Genepi Strasbourg
+    '';
+    ".signature.soniak".text = ''
+      Paul Hervot
+      pour l'association Sonia K
+    '';
   };
 
   # Protonmail
