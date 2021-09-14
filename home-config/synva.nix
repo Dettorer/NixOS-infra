2643a908a074ef17619778e45f5fbf0880cbab12
Paul Dettorer Hervot <dettorer@dettorer.net>
2021-09-14 14:28:30 +0200

    synva: add R and RStudio

--- home-config/synva.nix
+++ home-config/synva.nix
@@ -2,5 +2,9 @@
 
 {
   # Tools for SYNVA work
-  home.packages = with pkgs; [ _my.via ]; # TODO: no via on server
+  home.packages = with pkgs; [
+    _my.via # TODO: no via on server
+    rWrapper
+    rstudioWrapper
+  ];
 }
