3dfb6ff09f1f2ae436bd8ae652e6cce1a9302180
Paul Dettorer Hervot <dettorer@dettorer.net>
2022-03-05 15:55:00 +0100

    home-manager: follow the system's nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager }: {
