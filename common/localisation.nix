f0ca6de9fabff5da49e4a39cb39be08844e370d5
Paul "Dettorer" Hervot <dettorer@dettorer.net>
2023-07-23 16:20:46 +0200

    common: support all locales

--- common/localisation.nix
+++ common/localisation.nix
@@ -5,7 +5,7 @@
   time.timeZone = "Europe/Paris";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    supportedLocales = [ "en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8" ];
+    supportedLocales = [ "all" ];
     extraLocaleSettings = {
       "LC_TIME" = "fr_FR.UTF-8";
       "LC_MEASUREMENT" = "fr_FR.UTF-8";
