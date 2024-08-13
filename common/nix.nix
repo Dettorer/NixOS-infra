934ccbc7ba71bd82fca8fa03a871e721b2ec017c
Paul "Dettorer" Hervot <dettorer@dettorer.net>
2024-08-13 11:09:06 +0200

    nix: switch to lix

--- common/nix.nix
+++ common/nix.nix
@@ -10,7 +10,6 @@ in
     '';
     gc.automatic = true;
     gc.options = lib.mkDefault "--delete-older-than 14d";
-    package = pkgs.nixVersions.git;
     settings = {
       auto-optimise-store = true;
       trusted-users = [ "@wheel" ];
--- flake.nix
+++ flake.nix
@@ -1,18 +1,23 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, lix-module, home-manager }: {
     nixosConfigurations = {
       rivamar = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./machines/rivamar
+          lix-module.nixosModules.default
           home-manager.nixosModules.home-manager
         ];
       };
